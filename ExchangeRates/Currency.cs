feb859e6ad637507c2556cb4a76a2e56b4fc5d53
Vedran <vex.extreme@gmail.com>
2014-09-30 12:50:16 +0900

    Receive and ExchangeRates functionality

---
+++ ExchangeRates/Currency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Info.Blockchain.API.ExchangeRates
+{
+    /// <summary>
+    /// This class is used in the response of the `GetTicker` method in the `ExchangeRates` class.
+    /// </summary>
+    public  class Currency
+    {
+        public Currency(double buy, double sell, double last, double price15m, string symbol)
+        {
+            Buy = buy;
+            Sell = sell;
+            Last = last;
+            Price15m = price15m;
+            Symbol = symbol;
+        }
+
+        /// <summary>
+        /// Current buy price
+        /// </summary>
+        public double Buy { get; private set; }
+
+        /// <summary>
+        /// Current sell price
+        /// </summary>
+        public double Sell { get; private set; }
+
+        /// <summary>
+        /// Most recent market price
+        /// </summary>
+        public double Last { get; private set; }
+
+        /// <summary>
+        /// 15 minutes delayed market price
+        /// </summary>
+        public double Price15m { get; private set; }
+
+        /// <summary>
+        /// Currency symbol
+        /// </summary>
+        public string Symbol { get; private set; }
+    }
+}
---
+++ Receive/ReceiveResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Info.Blockchain.API.Receive
+{
+    /// <summary>
+    /// This class is used as a response object to the `Receive.receive` method. 
+    /// </summary>
+    public class ReceiveResponse
+    {
+        public ReceiveResponse(int feePercent, string destinationAddress,
+            string inputAddress, string callbackUrl)
+        {
+            FeePercent = feePercent;
+            DestinationAddress = destinationAddress;
+            InputAddress = inputAddress;
+            CallbackUrl = callbackUrl;
+        }
+
+        /// <summary>
+        /// Forwarding fee
+        /// </summary>
+        public int FeePercent { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DestinationAddress { get; private set; }
+
+        /// <summary>
+        /// Input address where the funds should be sent
+        /// </summary>
+        public string InputAddress { get; private set; }
+
+        /// <summary>
+        /// Callback URL that will be called upon payment
+        /// </summary>
+        public string CallbackUrl { get; private set; }
+    }
+}
